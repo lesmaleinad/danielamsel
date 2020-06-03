15d9e1886a96495b967d8a255d2c75df975a2f8b
lesmaleinad <lesmaleinad@hotmail.com>
2020-06-03 17:00:03 -0600

    update projects with pathfinder and danielamsel.com

--- public/src/components/Projects.js
+++ public/src/components/Projects.js
@@ -27,12 +27,22 @@ const Projects = () => (
                 </div>
             </div>
 
-            <div className="project">
+            <div className="pathfinder project">
                 <div className="project__front"></div>
                 <div className="project__body">
                     <div className="project__inner">
                         <h3 className="project__title">PathFinder</h3>
                         <p>Simple application implementing Dijkstra's algorithm for finding a path between 2 nodes. Built with Vanilla JS and JQuery.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="danielamsel project">
+                <div className="project__front"></div>
+                <div className="project__body">
+                    <div className="project__inner">
+                        <h3 className="project__title">This Website</h3>
+                        <p>Built with Express, Node, and React for tight response, controlled animation, and fast loading.</p>
                     </div>
                 </div>
             </div>
