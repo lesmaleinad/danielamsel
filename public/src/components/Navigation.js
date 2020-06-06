d9d004d0feab9a4924fb7a72802c90d3ea85161b
lesmaleinad <lesmaleinad@hotmail.com>
2020-06-05 23:29:37 -0600

    Modernize navigation layout

--- public/src/components/Navigation.js
+++ public/src/components/Navigation.js
@@ -6,8 +6,11 @@ export default class Navigation extends React.Component {
         return (
             <nav className={(!this.props.navOut ? 'intro--hold' : undefined)}>
                 <div className={'nav row flex-row justify-content-around flex-lg-column nav-pills '} id="nav-tab" role='tablist'>
-                    <img className="nav__img"  src=".\images\me.jpg" alt="A picture of Daniel Amsel"/>
-                    <h1 className="header-2 col-12 col-lg mt-lg-3">{this.props.header}</h1>
+                    <div className="nav__top">
+                        <img className="nav__img"  src=".\images\me.jpg" alt="A picture of Daniel Amsel"/>
+                        <h1 className="header-2 mt-lg-3">{this.props.header}</h1>
+                    </div>
+                    <hr />
                     {
                         this.props.pages.map((tab, i) => (
                             <NavLink
