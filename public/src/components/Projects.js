d742877e6a75d6d2343f712b35aee834ba0a5d92
Daniel Amsel <lesmaxleinad@gmail.com>
2022-10-24 20:01:19 -0600

    Use a symlink to connect pathfinder directly to SCM

--- public/src/components/Projects.js
+++ public/src/components/Projects.js
@@ -39,9 +39,9 @@ const Projects = () => (
 						<h3 className="project__title">PathFinder</h3>
 						<p>
 							Simple application implementing Dijkstra's algorithm for finding a path
-							between 2 nodes. Built with Vanilla JS and JQuery.
+							between 2 nodes. Built with Vanilla JS.
 						</p>
-						<a href="/pathfinder">danielamsel.com/pathfinder</a>
+						<a href="/pathfinder/path.html">danielamsel.com/pathfinder/path.html</a>
 					</div>
 				</div>
 			</div>
